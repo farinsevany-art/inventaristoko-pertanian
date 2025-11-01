b1e2cda2bd6f928095fdf88eff6d96a7e91ba3bd
alannnn32 <ahmadmaulana32321@gmail.com>
2025-11-01 21:17:24 +0700

    Update stok.php

--- toko/app/views/stok.php
+++ toko/app/views/stok.php
@@ -2,82 +2,58 @@
 <html>
 <?php include '../app/views/templates/header.php'; ?>
 
-<!-- STOK GUDANG -->
-<div class="cream_section layout_padding">
-   <div class="container">
-      <div class="row">
-         <div class="col-md-12">
-            <h1 class="cream_taital">Stok Gudang</h1>
-         </div>
-      </div>
+<div class="container mt-4">
+   <h1>Manajemen Stok</h1>
+   <p>Daftar stok gudang dan etalase. Gunakan tombol Edit / Hapus untuk memperbarui data (admin).</p>
 
-      <div class="cream_section_2">
-         <div class="row">
-            <?php if (!empty($stokGudang)) : ?>
-               <?php foreach ($stokGudang as $stok) : ?>
-                  <div class="col-md-4 mb-4">
-                     <div class="cream_box">
-                        <div class="cream_img">
-                           <?php 
-                              $imgFile = strtolower($stok['nama_barang']) . '.jpeg';
-                              $imgPath = 'images/' . (file_exists(__DIR__ . '/../../public/images/' . $imgFile) ? $imgFile : 'default.png');
-                           ?>
-                           <img src="<?= $imgPath ?>" alt="<?= htmlspecialchars($stok['nama_barang']) ?>" style="width: 200px;, height:200px;">
-                        </div>
-                        <div class="price_text"><?= htmlspecialchars($stok['total_stok_gudang']) ?> pcs</div>
-                        <h6 class="strawberry_text"><?= htmlspecialchars($stok['nama_barang']) ?></h6>
-                        <p class="lorem_text">Jenis: <?= htmlspecialchars($stok['jenis_barang']) ?><br>
-                           Kadaluarsa: <?= htmlspecialchars($stok['kadaluarsa_barang']) ?>
-                        </p>
-                        <div class="cart_bt"><a href="#">Edit Stok</a></div>
-                     </div>
-                  </div>
-               <?php endforeach; ?>
-            <?php else : ?>
-               <div class="col-md-12"><p>Tidak ada data stok gudang ditemukan.</p></div>
-            <?php endif; ?>
-         </div>
-      </div>
-   </div>
-</div>
+   <?php
+   // Merge stok arrays into one list with a source flag
+   $allStok = [];
+   if (!empty($stokGudang) && is_array($stokGudang)) {
+         foreach ($stokGudang as $s) { $s['source'] = 'gudang'; $allStok[] = $s; }
+   }
+   if (!empty($stokEtalase) && is_array($stokEtalase)) {
+         foreach ($stokEtalase as $s) { $s['source'] = 'etalase'; $allStok[] = $s; }
+   }
+   ?>
 
-<!-- STOK ETALASE -->
-<div class="cream_section layout_padding">
-   <div class="container">
-      <div class="row">
-         <div class="col-md-12">
-            <h1 class="cream_taital">Stok Etalase</h1>
-         </div>
-      </div>
+   <?php if (empty($allStok)) : ?>
+      <div class="alert alert-info">Tidak ada data stok tersedia.</div>
+   <?php else : ?>
+      <table class="table table-striped">
+         <thead>
+            <tr>
+               <th>#</th>
+               <th>Nama Barang</th>
+               <th>Jenis</th>
+               <th>Kadaluarsa</th>
+               <th>Lokasi</th>
+               <th>Jumlah</th>
+               <th>Aksi</th>
+            </tr>
+         </thead>
+         <tbody>
+            <?php foreach ($allStok as $i => $stok) :
+               $id = isset($stok['id']) ? urlencode($stok['id']) : urlencode($stok['nama_barang']);
+               $count = $stok['source'] === 'gudang' ? ($stok['total_stok_gudang'] ?? 0) : ($stok['total_stok_eta'] ?? 0);
+            ?>
+            <tr>
+               <td><?= $i+1 ?></td>
+               <td><?= htmlspecialchars($stok['nama_barang'] ?? '-') ?></td>
+               <td><?= htmlspecialchars($stok['jenis_barang'] ?? '-') ?></td>
+               <td><?= htmlspecialchars($stok['kadaluarsa_barang'] ?? '-') ?></td>
+               <td><?= htmlspecialchars($stok['source']) ?></td>
+               <td><?= htmlspecialchars($count) ?> pcs</td>
+               <td>
+                  <a class="btn btn-sm btn-primary" href="/crud/update.php?id=<?= $id ?>&source=<?= $stok['source'] ?>">Edit</a>
+                  <a class="btn btn-sm btn-danger" href="/crud/delete.php?id=<?= $id ?>&source=<?= $stok['source'] ?>" onclick="return confirm('Hapus stok <?= addslashes($stok['nama_barang'] ?? '') ?> dari <?= $stok['source'] ?>?')">Hapus</a>
+               </td>
+            </tr>
+            <?php endforeach; ?>
+         </tbody>
+      </table>
+   <?php endif; ?>
 
-      <div class="cream_section_2">
-         <div class="row">
-            <?php if (!empty($stokEtalase)) : ?>
-               <?php foreach ($stokEtalase as $stok) : ?>
-                  <div class="col-md-4 mb-4">
-                     <div class="cream_box">
-                        <div class="cream_img">
-                           <?php 
-                              $imgFile = strtolower($stok['nama_barang']) . '.jpeg';
-                              $imgPath = 'images/' . (file_exists(__DIR__ . '/../../public/images/' . $imgFile) ? $imgFile : 'default.png');
-                           ?>
-                           <img src="<?= $imgPath ?>" alt="<?= htmlspecialchars($stok['nama_barang']) ?>" style="width: 200px;, height:200px;">
-                        </div>
-                        <div class="price_text"><?= htmlspecialchars($stok['total_stok_eta']) ?> pcs</div>
-                        <h6 class="strawberry_text"><?= htmlspecialchars($stok['nama_barang']) ?></h6>
-                        <p class="lorem_text">Jenis: <?= htmlspecialchars($stok['jenis_barang']) ?><br>
-                           Kadaluarsa: <?= htmlspecialchars($stok['kadaluarsa_barang']) ?>
-                        </p>
-                        <div class="cart_bt"><a href="#">Edit Stok</a></div>
-                     </div>
-                  </div>
-               <?php endforeach; ?>
-            <?php else : ?>
-               <div class="col-md-12"><p>Tidak ada data stok etalase ditemukan.</p></div>
-            <?php endif; ?>
-         </div>
-      </div>
-   </div>
 </div>
 
 <?php include '../app/views/templates/footer.php'; ?>
