21e89dbab1b70b84e1d5d3a3c28fc83766c7f0e0
alannnn32 <ahmadmaulana32321@gmail.com>
2025-12-01 14:13:29 +0700

    Update HomeController.php

--- toko/app/controllers/HomeController.php
+++ toko/app/controllers/HomeController.php
@@ -15,4 +15,18 @@ class HomeController {
     public function stok() {
         require '../app/views/stok.php';
     }
+
+    // Forwarder: allow URLs like /home/barang/index to work by delegating
+    // to the BarangController. The front controller maps 'home/barang/index'
+    // to HomeController::barang('index'), so we dispatch here.
+    public function barang($action = 'index', ...$params) {
+        require_once __DIR__ . '/BarangController.php';
+        $ctrl = new BarangController();
+        if (method_exists($ctrl, $action)) {
+            call_user_func_array([$ctrl, $action], $params);
+        } else {
+            // fallback to index
+            $ctrl->index();
+        }
+    }
 }
